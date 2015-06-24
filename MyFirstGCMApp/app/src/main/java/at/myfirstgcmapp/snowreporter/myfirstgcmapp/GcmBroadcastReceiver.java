8f377dd8da70e44570cfb4b6f7f88eb77b9515a2
mfriedl89 <m.friedl89@gmail.com>
2015-06-24 13:16:17 +0200

    add new test-projects

--- MyFirstGCMApp/app/src/main/java/at/myfirstgcmapp/snowreporter/myfirstgcmapp/GcmBroadcastReceiver.java
+++ MyFirstGCMApp/app/src/main/java/at/myfirstgcmapp/snowreporter/myfirstgcmapp/GcmBroadcastReceiver.java
@@ -5,7 +5,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
-import android.widget.Toast;
 
 /**
  * Created by snowreporter on 06.05.2015.
--- MyFirstGCMApp/app/src/main/java/at/myfirstgcmapp/snowreporter/myfirstgcmapp/GcmIntentService.java
+++ MyFirstGCMApp/app/src/main/java/at/myfirstgcmapp/snowreporter/myfirstgcmapp/GcmIntentService.java
@@ -11,7 +11,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
