ed475c4f3bb488d59b156e6c1fc0074e3004c5ca
markusfriedl <m.friedl89@gmail.com>
2015-07-13 14:51:14 +0200

    some changes

---
+++ google-services-master/android/app-indexing/app/src/main/gen/com/google/developers/app_indexing/quickstart/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package com.google.developers.app_indexing.quickstart;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
